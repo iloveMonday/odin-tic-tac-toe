afc42d64db3eea6832601d85815e53850b90ae49
IloveMonday <iloveyouchandlerbing@gmail.com>
2024-05-01 14:31:04 -0400

    game working

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-
+function gameboard() {
     const board = [];
   
     for (let i = 0; i < 9; i++) {
-      board.push('');
+      board.push(i);
     }
   
     const getBoard = () => board;
@@ -10,9 +10,10 @@
     function makeMove(x) {
       if (board[x] === "X" || board[x] === "O") {
         console.log("naw man no way");
+        return;
       } else {
-        board[x] = getPlayerTurn().token;
-        switchPlayer();
+        board[x] = game.getPlayerTurn().token;
+        game.switchPlayer();
       }
     }
   
@@ -20,21 +21,30 @@
       if (
         (board[0] === board[1] && board[1] === board[2]) ||
         (board[3] === board[4] && board[4] === board[5]) ||
-        (board[6] === board[7] && board[7] === board[8])
-      ) {
-        console.log("WINNER");
-      }
+        (board[6] === board[7] && board[7] === board[8]) ||
+        (board[0] === board[3] && board[3] === board[6]) ||
+        (board[1] === board[4] && board[4] === board[7]) ||
+        (board[2] === board[5] && board[5] === board[8]) ||
+        (board[0] === board[4] && board[4] === board[8]) ||
+        (board[2] === board[4] && board[4] === board[6])
+      ) 
+        endGame();
+      
+    }
   
-    };
+    function endGame(){
+      game.switchPlayer();
+        let winner = game.getPlayerTurn().name;
+        console.log(winner + " is the WINNER");
+    }
   
+    return { getBoard, makeMove, checkWin, endGame };
+  }
   
-  
-  
-  
-
+  function gamePlay() {
     const playerOneName = "Toni";
     const playerTwoName = "Botoman";
-    // const board = gameboard();
+    //   const board = gameboard();
   
     const player = [
       {
@@ -60,71 +70,67 @@
   
     function playRound(x) {
       console.log("Now Playing: " + getPlayerTurn().name);
-      makeMove(x);
-      console.table(board.getBoard());
-      updateScreen();
-      checkWin();
+      gboard.makeMove(x);
+      screen.updateScreen();
+      // console.table(gboard.getBoard());
+      gboard.checkWin();
+      // game.switchPlayer();
+  
     }
   
+    return { getPlayerTurn, switchPlayer, playRound };
+  }
   
-  
-  
-  
-  
-  
-
+  function screenController() {
     function updateScreen() {
       let grid = document.getElementById("grid");
       let buttonWidth = grid.offsetWidth;
       let buttonSize = (buttonWidth / 3 / buttonWidth) * 100 + "%";
-      let boardy = getBoard();
+      let board = gboard.getBoard();
   
       grid.innerHTML = "";
-    
   
-      for (let i = 0; i < boardy.length; i++){
-          let newButton = document.createElement("button");
-          newButton.className = "board-button";
-          newButton.setAttribute("data-index", i);
+      for (let i = 0; i < board.length; i++) {
+        let newButton = document.createElement("button");
+        if (board[i] === "X" || board[i] === "O") {
           newButton.innerHTML = board[i];
-          newButton.style.height = buttonSize;
-          newButton.style.width = buttonSize;
-          grid.appendChild(newButton);
+        }
+        newButton.className = "board-button";
+        newButton.setAttribute("data-index", i);
+        // newButton.innerHTML = board[i];
+        newButton.style.height = buttonSize;
+        newButton.style.width = buttonSize;
+        grid.appendChild(newButton);
       }
+    }
+  
+    function handleClick() {
+      let grid = document.getElementById("grid");
+  
+      grid.addEventListener("click", (e) => {
+        game.playRound(e.target.dataset.index);
+        console.log(e.target.dataset.index);
+      });
+    }
+  
+    return {
+      updateScreen,
+      handleClick,
     };
+  }
   
-
-    let gridButton = document.querySelectorAll(".board-button")
-  
-      gridButton.forEach((e) =>{
-      e.addEventListener("click", () => {
-        console.log("hi");
-        //   playRound(e.dataset.index);
-          console.log(e.dataset.index);
-          // return e.dataset.index
-      });});
-    
-
-  
-  
-  
-  
-  
-  
-//   const gboard = gameboard();
-//   const game = gamePlay();
-//   const screen = screenController();
-  
-  
+  const gboard = gameboard();
+  const game = gamePlay();
+  const screen = screenController();
   
   let start = document.getElementById("start");
-  start.addEventListener("click", () =>{
-      updateScreen();
-  })
+  start.addEventListener("click", () => {
+    screen.updateScreen();
+  });
   
   
-  // game.playRound();
-  // screen.updateScreen();
-  // screen.handleClick();
+  screen.updateScreen();
+  screen.handleClick();
   
-  // gamePlay();
+  
+  
